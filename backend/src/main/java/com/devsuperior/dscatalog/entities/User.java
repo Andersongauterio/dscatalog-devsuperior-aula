13760e429d7665451a3e6de8755b7f2a270d1e75
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-06-06 12:06:10 -0300

    Basic BEAN validation

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -24,6 +24,7 @@ public class User implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String firstName;
 	private String lastName;
 	@Column(unique = true)
