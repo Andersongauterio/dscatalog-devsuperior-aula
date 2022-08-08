f49300d87954df63ee9b53407b0cc27b3adcd36d
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-08-08 20:18:02 -0300

    catalog - product details links

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import ProductCard from 'components/ProductCard';
+import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import './styles.css';
 
@@ -6,9 +7,11 @@ const Catalog = () => {
 	const product: Product = {
 		id: 2,
 		name: 'Smart TV',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		price: 2190.0,
-		imgUrl: 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
+		imgUrl:
+			'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
 		date: '2020-07-14T10:00:00Z',
 		categories: [
 			{
@@ -26,22 +29,34 @@ const Catalog = () => {
 		<div className="container my-4">
 			<div className="row">
 				<div className="col-sm-6 col-lg-4 col-xl-3">
-					<ProductCard product={product} />
+					<Link to="/products/1">
+						<ProductCard product={product} />
+					</Link>
 				</div>
 				<div className="col-sm-6 col-lg-4 col-xl-3">
-					<ProductCard product={product} />
+					<Link to="/products/1">
+						<ProductCard product={product} />
+					</Link>
 				</div>
 				<div className="col-sm-6 col-lg-4 col-xl-3">
-					<ProductCard product={product} />
+					<Link to="/products/1">
+						<ProductCard product={product} />
+					</Link>
 				</div>
 				<div className="col-sm-6 col-lg-4 col-xl-3">
-					<ProductCard product={product} />
+					<Link to="/products/1">
+						<ProductCard product={product} />
+					</Link>
 				</div>
 				<div className="col-sm-6 col-lg-4 col-xl-3">
-					<ProductCard product={product} />
+					<Link to="/products/1">
+						<ProductCard product={product} />
+					</Link>
 				</div>
 				<div className="col-sm-6 col-lg-4 col-xl-3">
-					<ProductCard product={product} />
+					<Link to="/products/1">
+						<ProductCard product={product} />
+					</Link>
 				</div>
 			</div>
 		</div>
