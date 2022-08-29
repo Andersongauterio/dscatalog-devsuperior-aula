e9948c9fc2d54915c03a5394404595ab7f6b8f94
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-08-29 20:08:32 -0300

    feat: get user authenticated

--- frontend/src/util/request.ts
+++ frontend/src/util/request.ts
@@ -1,19 +1,30 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import JwtDecode from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+	exp: number;
+	user_name: string;
+	authorities: Role[];
+};
 
 type LoginResponse = {
-       access_token: string;
-       token_type: string;
-       expires_in: number;
-       scope: string;
-       userFirstName: string
-       userId: number;
-}
+	access_token: string;
+	token_type: string;
+	expires_in: number;
+	scope: string;
+	userFirstName: string;
+	userId: number;
+};
 
 const tokenKey = 'authData';
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL =
+	process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
@@ -21,58 +32,84 @@ const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 //const basicHeader = () => 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET);
 
 type LoginData = {
-    username: string;
-    password: string;
-}
+	username: string;
+	password: string;
+};
 
-export const requestBackednLogin = (loginData : LoginData ) => {
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
-    };
+export const requestBackednLogin = (loginData: LoginData) => {
+	const headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+	};
 
-    const data = qs.stringify( {
-        ...loginData,
-        grant_type : 'password'
-    });
+	const data = qs.stringify({
+		...loginData,
+		grant_type: 'password',
+	});
 
-    return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
-
-}
+	return axios({
+		method: 'POST',
+		baseURL: BASE_URL,
+		url: '/oauth/token',
+		data,
+		headers,
+	});
+};
 
 export const requestBackend = (config: AxiosRequestConfig) => {
+	const headers = config.withCredentials
+		? {
+				...config.headers,
+				Authorization: 'Bearer ' + getAuthData().access_token,
+		  }
+		: config.headers;
 
-    const headers = config.withCredentials ? {
-        ...config.headers,
-        Authorization: "Bearer " + getAuthData().access_token
-    } : config.headers;
+	return axios({ ...config, baseURL: BASE_URL, headers });
+};
 
-    return axios({...config, baseURL: BASE_URL, headers});
-}
-
-export const saveAuthData = (obj : LoginResponse) => {
-    localStorage.setItem(tokenKey, JSON.stringify(obj));
-}
+export const saveAuthData = (obj: LoginResponse) => {
+	localStorage.setItem(tokenKey, JSON.stringify(obj));
+};
 
 export const getAuthData = () => {
-    const str = localStorage.getItem(tokenKey) ?? "{}";
-    return JSON.parse(str) as LoginResponse;
-}
+	const str = localStorage.getItem(tokenKey) ?? '{}';
+	return JSON.parse(str) as LoginResponse;
+};
 
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
-    return config;
-  }, function (error) {
-    return Promise.reject(error);
-  });
+axios.interceptors.request.use(
+	function (config) {
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error);
+	}
+);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-    //
-    return response;
-  }, function (error) {
-      if(error.response.status === 401 || error.response.status === 403) {
-        history.push('/admin/auth');
-      }
-    return Promise.reject(error);
-  });
+axios.interceptors.response.use(
+	function (response) {
+		//
+		return response;
+	},
+	function (error) {
+		if (error.response.status === 401 || error.response.status === 403) {
+			history.push('/admin/auth');
+		}
+		return Promise.reject(error);
+	}
+);
+
+export const getTokenData = (): TokenData | undefined => {
+	try {
+		return jwtDecode(getAuthData().access_token) as TokenData;
+	} catch (error) {
+		return undefined;
+	}
+}
+
+export const isAuthenticated = () : boolean => {
+    const tokenData = getTokenData();
+
+    return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+}
