400f9a8b0779e6fa475c17a0d2b34c24d5323819
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-09-27 21:02:30 -0300

    Product routes

--- frontend/src/components/PrivateRoute/index.tsx
+++ frontend/src/components/PrivateRoute/index.tsx
@@ -22,7 +22,7 @@ const PrivateRoute = ({ children, path, roles = [] }: Props) => {
 				) : !hasAnyRoles(roles) ? (
 					<Redirect to="/admin/products" />
 				) : (
-					<>children</>
+					<>{children}</>
 				)
 			}
 		/>
