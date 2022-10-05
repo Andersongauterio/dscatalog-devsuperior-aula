2ce006930d25282296da5b58c5b20913055d5ff4
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-10-04 21:22:46 -0300

    ProductCrudForm HTML

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -25,7 +25,7 @@ const List = () => {
 	};
 
 	return (
-		<div>
+		<div className="product-crud-container">
 			<div className="product-crud-bar-container">
 				<Link to="/admin/products/create">
 					<button className="btn btn-primary text-white btn-crud-add">
